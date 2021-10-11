551776ca183cf822e1acf508dbef2c7df4d3bc53
nadiaroschina <nadysha.rr@gmail.com>
2021-10-11 22:40:23 +0300

    usage updated & exceptions renamed

--- src/main/kotlin/Exceptions.kt
+++ src/main/kotlin/Exceptions.kt
@@ -2,7 +2,7 @@ class EmptyQuery() :
     Exception("Empty query")
 
 class InvalidArguments(args: List<String>) :
-    Exception("Invalid number of arguments: cant separate $args into pairs of key and value")
+    Exception("cant separate $args into pairs of key and value")
 
 class UnsupportedQuery(queryName: String) :
-    Exception("Query $queryName is not supported")
+    Exception("\"$queryName\" is not supported")
